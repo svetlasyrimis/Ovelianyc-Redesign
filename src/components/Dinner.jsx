7d7a8dfbca8c626ef386c15e64fe962df9ea5c49
Nazrul Babu <n95babu@gmail.com>
2019-07-29 14:46:06 -0400

    Dinner Data

---
+++ src/components/Dinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Dinner extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the Dinner Page </h1>
+      </div>
+    )
+  }
+}
